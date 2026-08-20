97b18699f7a70d32976410df4ad307bd4ed46aee
Aryan Hapaliya <hapaliyaaryan24@gmail.com>
2026-08-20 16:08:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let option = document.querySelector(".option");
+let pss = document.querySelector(".p");
+let select = document.querySelector("#selectField");
+let arrow = document.querySelector("#arrow");
+let content = document.querySelector("#content");
+
+select.addEventListener("click", () => {
+    if(option.style.visibility == "visible"){
+        arrow.style.transform = "rotate(0deg)";
+        option.style.visibility = "hidden";
+    } else {
+        arrow.style.transform = "rotate(180deg)";
+        option.style.visibility = "visible";
+    }
+})
+
+p.addEventListener('click',(e) => {
+    e.preventDefault();
+    content.innerHTML = e.p;
+    console.log(e);
+})
